31384d87edac97c95e04b066fcc48905ae750f45
Ivan Krakhmalyuk (LifeDJIK, lifedj27) <lifedj27@gmail.com>
2013-02-18 20:39:37 +0200

    Testing default.prop

--- device_m805_892x.mk
+++ device_m805_892x.mk
@@ -21,3 +21,6 @@ $(call inherit-product, build/target/product/full.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_m805_892x
 PRODUCT_DEVICE := m805_892x
+
+PRODUCT_PROPERTY_OVERRIDES := \
+    persist.sys.usb.config=mass_storage,adb
