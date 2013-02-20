40f5f5fa57b155384406c36b60b5fecd6a4ae72f
Ivan Krakhmalyuk (LifeDJIK, lifedj27) <lifedj27@gmail.com>
2013-02-20 17:24:13 +0200

    Fix product name

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, device/targa/utab_7_6/device_utab_7_6.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := m805_892x
-PRODUCT_NAME := cm_m805_892x
+PRODUCT_NAME := cm_utab_7_6
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := Targa uTab 7.6
 PRODUCT_MANUFACTURER := unknown
